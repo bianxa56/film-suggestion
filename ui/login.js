464044dc29601eafe825d9ad7a6b0624e66b0002
bianxa56 <71631842+bianxa56@users.noreply.github.com>
2022-12-13 16:25:14 +0800

    integrated login api in login page

--- ui/login.js
+++ ui/login.js
@@ -1,7 +1,7 @@
 btnSubmit = document.getElementById("btn-submit")
 btnSubmit.addEventListener("click", onSubmitClick);
 
-function onSubmitClick() {
+async function onSubmitClick() {
     username = document.getElementById("txt-username").value
     password = document.getElementById("txt-password").value
 
@@ -9,4 +9,7 @@ function onSubmitClick() {
         username, password
     }
     console.log(request)
+    url = '/api/film_suggestion/server/resource/login-resource.php'
+    response = await axios.post(url, request)
+    alert(response.data)
 }
